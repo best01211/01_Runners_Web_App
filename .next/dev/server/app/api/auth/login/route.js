f68699cc9dc445bcc8b255eede6e0d5ec31735bc
best01211 <jinwoobest.andy01@gmail.com>
2026-08-05 20:16:22 +0900

    서버 디버그 모드 수정
    디버그 모드가 실행되면서 서버 자원을 잡아먹고 있는 상태를 수정

--- .next/dev/server/app/api/auth/login/route.js
+++ .next/dev/server/app/api/auth/login/route.js
@@ -1,5 +1,5 @@
 var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/login/route.js")
-R.c("server/chunks/[project]_src_app_api_auth_login_route_ts_1tm7fug._.js")
+R.c("server/chunks/src_app_api_auth_login_route_ts_1tm7fug._.js")
 R.c("server/chunks/node_modules_next_0qu0_50._.js")
 R.c("server/chunks/node_modules_@supabase_auth-js_dist_module_00x2u05._.js")
 R.c("server/chunks/node_modules_05-6_yr._.js")
--- .next/dev/server/app/api/auth/signup/route.js
+++ .next/dev/server/app/api/auth/signup/route.js
@@ -1,5 +1,5 @@
 var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/signup/route.js")
-R.c("server/chunks/[project]_src_app_api_auth_signup_route_ts_0zyepss._.js")
+R.c("server/chunks/src_app_api_auth_signup_route_ts_0zyepss._.js")
 R.c("server/chunks/node_modules_next_0l5hic4._.js")
 R.c("server/chunks/node_modules_@supabase_auth-js_dist_module_00x2u05._.js")
 R.c("server/chunks/node_modules_05-6_yr._.js")
